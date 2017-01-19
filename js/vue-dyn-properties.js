049f41ea1f75314a94128cc73e8e37915f8b9653
foy <ljs2342003@gmail.com>
2017-01-19 14:58:59 +0800

    refatcor

--- js/vue-dyn-properties.js
+++ js/vue-dyn-properties.js
@@ -36,12 +36,15 @@ Vue.component("dyn", {
             border: 2px solid #E4393C;
             background: url(/images/inc-02.png) #FFF no-repeat right bottom;
         }
+        .disable a {
+          background: gray;  
+        }
     </style>
     
     <div class="charge grayBg" v-for="(optionKey,optionValue) in options">
     <span class="title" >{{optionKey}}：</span>
        <ul>
-         <li :class="{'active':isActive(optionKey,value)}" v-for="value in optionValue" @click="choose(optionKey,value)"><a href="#">{{value}}</a></li>
+         <li :class="{'active':isActive(optionKey,value),'disable':isDisable(optionKey,value)}" v-for="value in optionValue" @click="choose(optionKey,value)"><a href="#">{{value}}</a></li>
        </ul>
     </div>
     
@@ -87,13 +90,32 @@ Vue.component("dyn", {
     },
     methods: {
         choose: function (key, value) {
-            Vue.set(this.selected, key, value);
+            if(_.has(this.selected,key) && value == this.selected[key]){
+                Vue.set(this.selected, key, "");
+            }else{
+                Vue.set(this.selected, key, value);
+            }
         },
         isActive: function (key, value) {
             if (_.has(this.selected, key) && value == this.selected[key]) {
                 return true;
             }
             return false;
+        },
+        isDisable: function (key, value) {
+            var filter = {};
+            if (!_.isEmpty(this.selected)) {
+                _.each(this.selected, function (pValue, pKey) {
+                    filter[pKey] = pValue;
+                })
+            }
+            filter[key] = value;
+            if(!_.isEmpty(_.filter(this.skus, function (sku) {
+                return _.where([JSON.parse(JSON.stringify(sku.dyn))], filter).length > 0;
+            }))){
+                return false;
+            }
+            return true;
         },
         kvPair: function (key, value) {
             return key + '_' + value;
